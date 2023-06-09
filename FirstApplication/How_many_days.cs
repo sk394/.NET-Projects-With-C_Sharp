a0f1046182c5d676868374659d8119ec51cd8ce7
Suman Khadka <sk394@uakron.edu>
2023-06-09 12:51:34 -0400

    Add project files.

---
+++ FirstApplication/How_many_days.cs
@@ -0,0 +1,39 @@
+﻿DateTime today = DateTime.Today;
+string date = " ";
+do
+{
+    Console.WriteLine("Enter the date or type \"exit\" ");
+    date = Console.ReadLine();
+    DateTime parsed;
+    TimeSpan ts;
+
+    if ( date == "exit")
+    {
+        break;
+    }
+    {
+        
+    }
+    if (DateTime.TryParse(date, out parsed))
+    {
+        if(parsed < today)
+        {
+            ts = today - parsed;
+            Console.WriteLine($"That day went by {ts.Days} days" );
+        }else if(parsed > today)
+        {
+            ts = parsed - today;
+            Console.WriteLine( $"This day will be in {ts.Days} days");
+        }
+        else
+        {
+            Console.WriteLine( $"This day is today");
+        }
+
+    }
+    else
+    {
+        Console.WriteLine($"{date, -10}This is not a valid day: Try again");
+    }
+
+} while (date != "exit");
